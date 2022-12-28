7655ef2e920eb6375df98ccfaca40ebca53cd07f
alammooo <abdullah.alamm@gmail.com>
2022-12-28 13:25:24 +0700

    add animation on card

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -82,13 +82,41 @@ export function HomePage() {
           </h1>
         </div> */}
         <div className="grid grid-cols-6 gap-2 px-2 mt-3">
-          <div className="overflow-hidden">
-            <img
-              className="object-cover h-full w-full hover:scale-105 duration-500"
-              src="https://img.freepik.com/free-photo/pura-taman-ayun-temple-bali-indonesia_335224-392.jpg?w=1380&t=st=1672206056~exp=1672206656~hmac=bb75d482494deca58cef3e09ca5236a0569a8a5c920f2cc12745b0619fbc4222"
-              alt="pura bali"
-            />
-          </div>
+          <div
+            className=" 
+                overflow-hidden 
+              bg-white 
+                flex 
+                flex-col 
+                items-center 
+                w-full 
+                p-10
+                relative
+              hover:text-white
+                before:contents-['']
+                before:top-0
+                before:left-0
+                before:bottom-0
+                before:right-0
+                before:-z-10
+                before:bg-zinc-900
+                before:bg-opacity-25
+                before:transition-all
+                before:duration-500
+                before:ease-out
+                before:scale-y-0
+                before:absolute
+                before:hover:scale-y-100
+                before:focus:scale-y-100
+                
+                transition-colors
+                duration-500
+                ease-out
+                z-10
+                bg-cover
+                bg-[url('https://img.freepik.com/free-photo/pura-taman-ayun-temple-bali-indonesia_335224-392.jpg?w=1380&t=st=1672206056~exp=1672206656~hmac=bb75d482494deca58cef3e09ca5236a0569a8a5c920f2cc12745b0619fbc4222')]
+                
+                "></div>
           <div className="row-span-2 col-span-2 overflow-hidden">
             <img
               className="object-cover h-full w-full hover:scale-105 duration-500"
