9bf28722e180d899f5048d0b11b9a5e45249a2c3
alammooo <abdullah.alamm@gmail.com>
2023-01-01 22:26:51 +0700

    add logo on assets

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,94 @@
 export default function Cards() {
   return (
-    <div className="grid grid-cols-4 gap-2 px-2 mt-3">
+    <div className="grid grid-cols-6 gap-2 px-2 mt-3">
+      <div className="overflow-hidden relative group cursor-pointer ">
+        <div
+          className="
+                aspect-square
+                overflow-hidden 
+                w-full 
+                before:contents-['']
+                before:top-0
+                before:left-0
+                before:bottom-0
+                before:right-0
+                before:z-20
+              before:bg-zinc-900
+                before:bg-opacity-25
+                before:transition-all
+                before:duration-500
+                before:ease-out
+                before:scale-y-0
+                before:absolute
+                before:hover:scale-y-100
+                before:focus:scale-y-100
+                
+                duration-300
+                ease-out
+                z-10
+                
+                ">
+          <img
+            className="object-cover h-full w-full group-hover:scale-105 duration-700"
+            src="https://img.freepik.com/free-photo/pura-taman-ayun-temple-bali-indonesia_335224-392.jpg?w=1380&t=st=1672206056~exp=1672206656~hmac=bb75d482494deca58cef3e09ca5236a0569a8a5c920f2cc12745b0619fbc4222"
+            alt="pura bali"
+          />
+
+          <h1 className="text-5xl text-white font-semibold absolute bottom-7 left-5 z-20 backdrop-blur-sm px-2 py-1">
+            Pura taman Ayun
+          </h1>
+        </div>
+          <div className="group-hover:opacity-100 absolute top-0 translate-x-1/2 translate-y-1/2 opacity-0 duration-500 transition-all z-20 flex-col flex gap-10">
+            <h1 className="text-2xl text-white font-semibold">Internet</h1>
+            <h1 className="text-2xl text-white font-semibold">Fun</h1>
+            <h1 className="text-2xl text-white font-semibold">Price</h1>
+            <h1 className="text-2xl text-white font-semibold">Safety</h1>
+          </div>
+      </div>
+      <div className="overflow-hidden relative group cursor-pointer col-span-2 row-span-2 aspect-square">
+        <div
+          className="
+                aspect-square
+                overflow-hidden 
+                w-full 
+                before:contents-['']
+                before:top-0
+                before:left-0
+                before:bottom-0
+                before:right-0
+                before:z-20
+              before:bg-zinc-900
+                before:bg-opacity-25
+                before:transition-all
+                before:duration-500
+                before:ease-out
+                before:scale-y-0
+                before:absolute
+                before:hover:scale-y-100
+                before:focus:scale-y-100
+                
+                duration-300
+                ease-out
+                z-10
+                
+                ">
+          <img
+            className="object-cover h-full w-full group-hover:scale-105 duration-700"
+            src="https://img.freepik.com/free-photo/pura-taman-ayun-temple-bali-indonesia_335224-392.jpg?w=1380&t=st=1672206056~exp=1672206656~hmac=bb75d482494deca58cef3e09ca5236a0569a8a5c920f2cc12745b0619fbc4222"
+            alt="pura bali"
+          />
+
+          <h1 className="text-5xl text-white font-semibold absolute bottom-7 left-5 z-20 backdrop-blur-sm px-2 py-1">
+            Pura taman Ayun
+          </h1>
+        </div>
+          <div className="group-hover:opacity-100 absolute top-0 translate-x-1/2 translate-y-1/2 opacity-0 duration-500 transition-all z-20 flex-col flex gap-10">
+            <h1 className="text-2xl text-white font-semibold">Internet</h1>
+            <h1 className="text-2xl text-white font-semibold">Fun</h1>
+            <h1 className="text-2xl text-white font-semibold">Price</h1>
+            <h1 className="text-2xl text-white font-semibold">Safety</h1>
+          </div>
+      </div>
       <div className="overflow-hidden relative group cursor-pointer">
         <div
           className="
@@ -45,6 +133,272 @@ export default function Cards() {
             <h1 className="text-2xl text-white font-semibold">Safety</h1>
           </div>
       </div>
+      <div className="overflow-hidden relative group cursor-pointer">
+        <div
+          className="
+                aspect-square
+                overflow-hidden 
+                w-full 
+                before:contents-['']
+                before:top-0
+                before:left-0
+                before:bottom-0
+                before:right-0
+                before:z-20
+              before:bg-zinc-900
+                before:bg-opacity-25
+                before:transition-all
+                before:duration-500
+                before:ease-out
+                before:scale-y-0
+                before:absolute
+                before:hover:scale-y-100
+                before:focus:scale-y-100
+                
+                duration-300
+                ease-out
+                z-10
+                
+                ">
+          <img
+            className="object-cover h-full w-full group-hover:scale-105 duration-700"
+            src="https://img.freepik.com/free-photo/pura-taman-ayun-temple-bali-indonesia_335224-392.jpg?w=1380&t=st=1672206056~exp=1672206656~hmac=bb75d482494deca58cef3e09ca5236a0569a8a5c920f2cc12745b0619fbc4222"
+            alt="pura bali"
+          />
+
+          <h1 className="text-5xl text-white font-semibold absolute bottom-7 left-5 z-20 backdrop-blur-sm px-2 py-1">
+            Pura taman Ayun
+          </h1>
+        </div>
+          <div className="group-hover:opacity-100 absolute top-0 translate-x-1/2 translate-y-1/2 opacity-0 duration-500 transition-all z-20 flex-col flex gap-10">
+            <h1 className="text-2xl text-white font-semibold">Internet</h1>
+            <h1 className="text-2xl text-white font-semibold">Fun</h1>
+            <h1 className="text-2xl text-white font-semibold">Price</h1>
+            <h1 className="text-2xl text-white font-semibold">Safety</h1>
+          </div>
+      </div>
+      <div className="overflow-hidden relative group cursor-pointer">
+        <div
+          className="
+                aspect-square
+                overflow-hidden 
+                w-full 
+                before:contents-['']
+                before:top-0
+                before:left-0
+                before:bottom-0
+                before:right-0
+                before:z-20
+              before:bg-zinc-900
+                before:bg-opacity-25
+                before:transition-all
+                before:duration-500
+                before:ease-out
+                before:scale-y-0
+                before:absolute
+                before:hover:scale-y-100
+                before:focus:scale-y-100
+                
+                duration-300
+                ease-out
+                z-10
+                
+                ">
+          <img
+            className="object-cover h-full w-full group-hover:scale-105 duration-700"
+            src="https://img.freepik.com/free-photo/pura-taman-ayun-temple-bali-indonesia_335224-392.jpg?w=1380&t=st=1672206056~exp=1672206656~hmac=bb75d482494deca58cef3e09ca5236a0569a8a5c920f2cc12745b0619fbc4222"
+            alt="pura bali"
+          />
+
+          <h1 className="text-5xl text-white font-semibold absolute bottom-7 left-5 z-20 backdrop-blur-sm px-2 py-1">
+            Pura taman Ayun
+          </h1>
+        </div>
+          <div className="group-hover:opacity-100 absolute top-0 translate-x-1/2 translate-y-1/2 opacity-0 duration-500 transition-all z-20 flex-col flex gap-10">
+            <h1 className="text-2xl text-white font-semibold">Internet</h1>
+            <h1 className="text-2xl text-white font-semibold">Fun</h1>
+            <h1 className="text-2xl text-white font-semibold">Price</h1>
+            <h1 className="text-2xl text-white font-semibold">Safety</h1>
+          </div>
+      </div>
+      <div className="overflow-hidden relative group cursor-pointer row-span-2 h-3/4">
+        <div
+          className="
+                aspect-square
+                overflow-hidden 
+                w-full 
+                before:contents-['']
+                before:top-0
+                before:left-0
+                before:bottom-0
+                before:right-0
+                before:z-20
+              before:bg-zinc-900
+                before:bg-opacity-25
+                before:transition-all
+                before:duration-500
+                before:ease-out
+                before:scale-y-0
+                before:absolute
+                before:hover:scale-y-100
+                before:focus:scale-y-100
+                
+                duration-300
+                ease-out
+                z-10
+                
+                ">
+          <img
+            className="object-cover h-full w-full group-hover:scale-105 duration-700"
+            src="https://img.freepik.com/free-photo/pura-taman-ayun-temple-bali-indonesia_335224-392.jpg?w=1380&t=st=1672206056~exp=1672206656~hmac=bb75d482494deca58cef3e09ca5236a0569a8a5c920f2cc12745b0619fbc4222"
+            alt="pura bali"
+          />
+
+          <h1 className="text-5xl text-white font-semibold absolute bottom-7 left-5 z-20 backdrop-blur-sm px-2 py-1">
+            Pura taman Ayun
+          </h1>
+        </div>
+          <div className="group-hover:opacity-100 absolute top-0 translate-x-1/2 translate-y-1/2 opacity-0 duration-500 transition-all z-20 flex-col flex gap-10">
+            <h1 className="text-2xl text-white font-semibold">Internet</h1>
+            <h1 className="text-2xl text-white font-semibold">Fun</h1>
+            <h1 className="text-2xl text-white font-semibold">Price</h1>
+            <h1 className="text-2xl text-white font-semibold">Safety</h1>
+          </div>
+      </div>
+      <div className="overflow-hidden relative group cursor-pointer col-span-2">
+        <div
+          className="
+                aspect-square
+                overflow-hidden 
+                w-full 
+                before:contents-['']
+                before:top-0
+                before:left-0
+                before:bottom-0
+                before:right-0
+                before:z-20
+              before:bg-zinc-900
+                before:bg-opacity-25
+                before:transition-all
+                before:duration-500
+                before:ease-out
+                before:scale-y-0
+                before:absolute
+                before:hover:scale-y-100
+                before:focus:scale-y-100
+                
+                duration-300
+                ease-out
+                z-10
+                
+                ">
+          <img
+            className="object-cover h-full w-full group-hover:scale-105 duration-700"
+            src="https://img.freepik.com/free-photo/pura-taman-ayun-temple-bali-indonesia_335224-392.jpg?w=1380&t=st=1672206056~exp=1672206656~hmac=bb75d482494deca58cef3e09ca5236a0569a8a5c920f2cc12745b0619fbc4222"
+            alt="pura bali"
+          />
+
+          <h1 className="text-5xl text-white font-semibold absolute bottom-7 left-5 z-20 backdrop-blur-sm px-2 py-1">
+            Pura taman Ayun
+          </h1>
+        </div>
+          <div className="group-hover:opacity-100 absolute top-0 translate-x-1/2 translate-y-1/2 opacity-0 duration-500 transition-all z-20 flex-col flex gap-10">
+            <h1 className="text-2xl text-white font-semibold">Internet</h1>
+            <h1 className="text-2xl text-white font-semibold">Fun</h1>
+            <h1 className="text-2xl text-white font-semibold">Price</h1>
+            <h1 className="text-2xl text-white font-semibold">Safety</h1>
+          </div>
+      </div>
+      <div className="overflow-hidden relative group cursor-pointer row-span-2">
+        <div
+          className="
+                aspect-square
+                overflow-hidden 
+                w-full 
+                before:contents-['']
+                before:top-0
+                before:left-0
+                before:bottom-0
+                before:right-0
+                before:z-20
+              before:bg-zinc-900
+                before:bg-opacity-25
+                before:transition-all
+                before:duration-500
+                before:ease-out
+                before:scale-y-0
+                before:absolute
+                before:hover:scale-y-100
+                before:focus:scale-y-100
+                
+                duration-300
+                ease-out
+                z-10
+                
+                ">
+          <img
+            className="object-cover h-full w-full group-hover:scale-105 duration-700"
+            src="https://img.freepik.com/free-photo/pura-taman-ayun-temple-bali-indonesia_335224-392.jpg?w=1380&t=st=1672206056~exp=1672206656~hmac=bb75d482494deca58cef3e09ca5236a0569a8a5c920f2cc12745b0619fbc4222"
+            alt="pura bali"
+          />
+
+          <h1 className="text-5xl text-white font-semibold absolute bottom-7 left-5 z-20 backdrop-blur-sm px-2 py-1">
+            Pura taman Ayun
+          </h1>
+        </div>
+          <div className="group-hover:opacity-100 absolute top-0 translate-x-1/2 translate-y-1/2 opacity-0 duration-500 transition-all z-20 flex-col flex gap-10">
+            <h1 className="text-2xl text-white font-semibold">Internet</h1>
+            <h1 className="text-2xl text-white font-semibold">Fun</h1>
+            <h1 className="text-2xl text-white font-semibold">Price</h1>
+            <h1 className="text-2xl text-white font-semibold">Safety</h1>
+          </div>
+      </div>
+      <div className="overflow-hidden relative group cursor-pointer col-span-2">
+        <div
+          className="
+                aspect-square
+                overflow-hidden 
+                w-full 
+                before:contents-['']
+                before:top-0
+                before:left-0
+                before:bottom-0
+                before:right-0
+                before:z-20
+              before:bg-zinc-900
+                before:bg-opacity-25
+                before:transition-all
+                before:duration-500
+                before:ease-out
+                before:scale-y-0
+                before:absolute
+                before:hover:scale-y-100
+                before:focus:scale-y-100
+                
+                duration-300
+                ease-out
+                z-10
+                
+                ">
+          <img
+            className="object-cover h-full w-full group-hover:scale-105 duration-700"
+            src="https://img.freepik.com/free-photo/pura-taman-ayun-temple-bali-indonesia_335224-392.jpg?w=1380&t=st=1672206056~exp=1672206656~hmac=bb75d482494deca58cef3e09ca5236a0569a8a5c920f2cc12745b0619fbc4222"
+            alt="pura bali"
+          />
+
+          <h1 className="text-5xl text-white font-semibold absolute bottom-7 left-5 z-20 backdrop-blur-sm px-2 py-1">
+            Pura taman Ayun
+          </h1>
+        </div>
+          <div className="group-hover:opacity-100 absolute top-0 translate-x-1/2 translate-y-1/2 opacity-0 duration-500 transition-all z-20 flex-col flex gap-10">
+            <h1 className="text-2xl text-white font-semibold">Internet</h1>
+            <h1 className="text-2xl text-white font-semibold">Fun</h1>
+            <h1 className="text-2xl text-white font-semibold">Price</h1>
+            <h1 className="text-2xl text-white font-semibold">Safety</h1>
+          </div>
+      </div>
     </div>
+
+
   )
 }
