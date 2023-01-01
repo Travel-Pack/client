601420075a6c24a6592a050081a19b3db52f3706
Joshua Natanael <joshuanatanael@Joshuas-MacBook-Air.local>
2023-01-01 20:36:38 +0700

    add routes, login page template, register page tamplate, replace html title

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from "react"
 import Navbar from "./components/Navbar"
 
 import { Counter } from "./features/counter/Counter"
-import { HomePage } from "./pages/HomePage"
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <div className="App"></div>
   )
 }
 
