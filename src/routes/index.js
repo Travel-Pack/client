4b8c58ca57b695828f2d1085c48c22a46610a33d
alammooo <abdullah.alamm@gmail.com>
2023-01-02 17:14:56 +0700

    adding error Page and route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,28 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom"
 import { HomePage } from "../pages/HomePage"
 import { LoginPage } from "../pages/LoginPage"
 import { RegisterPage } from "../pages/RegisterPage"
-import { DestinationDetailPage } from "../pages/DestinationDetailPage";
-import { DestinationInformation } from "../components/DestinationDetailPage/DestinationInformation";
-import { DestinationReview } from "../components/DestinationDetailPage/DestinationReview";
-import { DestinationCovid } from "../components/DestinationDetailPage/DestinationCovid";
+import { DestinationDetailPage } from "../pages/DestinationDetailPage"
+import { DestinationInformation } from "../components/DestinationDetailPage/DestinationInformation"
+import { DestinationReview } from "../components/DestinationDetailPage/DestinationReview"
+import { DestinationCovid } from "../components/DestinationDetailPage/DestinationCovid"
+import ErrorPage from "../pages/ErrorPage"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/login",
     element: <LoginPage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/register",
     element: <RegisterPage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/destination/:destinationName/",
@@ -27,17 +31,20 @@ const router = createBrowserRouter([
       {
         path: "information",
         element: <DestinationInformation />,
+        errorElement: <ErrorPage />,
       },
       {
         path: "review",
         element: <DestinationReview />,
+        errorElement: <ErrorPage />,
       },
       {
         path: "covid",
         element: <DestinationCovid />,
+        errorElement: <ErrorPage />,
       },
-    ]
+    ],
   },
-]);
+])
 
-export default router;
+export default router
