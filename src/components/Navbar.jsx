4ddcf9e26ef689c9452f003490e4d928eaebbea2
alammooo <abdullah.alamm@gmail.com>
2023-01-06 04:50:23 +0700

    add travel step section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,26 +22,39 @@ export default function Navbar() {
 
   return (
     <div>
-      <div className={`flex bg-white bg-opacity-90 items-center justify-between fixed w-full px-20 shadow-sm backdrop-blur-sm duration-200 h-[80px] ${visible ? 'top-0' : '-top-[80px]'}`}>
+      <div
+        className={`flex bg-white bg-opacity-90 items-center z-50 justify-between fixed w-full px-20 shadow-sm backdrop-blur-md duration-200 h-[80px] ${
+          visible ? "top-0" : "-top-[80px]"
+        }`}>
         <NavLink to={"/"} className="text-2xl">
           Travel <span className="font-bold">Pack</span>
         </NavLink>
         <div className="flex justify-center gap-10">
           <div>
-            <NavLink to={"/"}>Home</NavLink>
+            <NavLink className="font-medium hover:border-b-4 border-yelloku duration-400" to={"/"}>
+              Home
+            </NavLink>
           </div>
           <div>
-            <NavLink to={"/travel-step"}>Travel Step</NavLink>
+            <NavLink className="font-medium hover:border-b-4 border-yelloku duration-400" to={"/travel-step"}>
+              Travel Step
+            </NavLink>
           </div>
           <div>
-            <NavLink to={"/find-travel"}>Find Tour</NavLink>
+            <NavLink className="font-medium hover:border-b-4 border-yelloku duration-400" to={"/find-travel"}>
+              Find Tour
+            </NavLink>
+          </div>
+          <div className="mr-16">
+            <NavLink className="font-medium hover:border-b-4 border-yelloku duration-400" to={"forum"}>
+              Forum
+            </NavLink>
           </div>
           <div>
-            <NavLink to={"forum"}>Forum</NavLink>
+            <NavLink className="font-medium hover:border-b-4 border-yelloku duration-400" to={"/register"}>
+              Register
+            </NavLink>
           </div>
-        </div>
-        <div className="flex justify-center gap-2">
-          <NavLink to={"/register"}>Register</NavLink>
         </div>
       </div>
     </div>
--- src/pages/TravelStep.jsx
+++ src/pages/TravelStep.jsx
@@ -1,34 +1,75 @@
+import { useState } from "react"
 import SwiperCar from "../components/Swiper/SwiperCar"
 
 export default function TravelStep() {
+  const [sliderVal, setSliderVal] = useState(0)
+
   function handleSort() {
     console.log("blabla")
   }
   return (
-    <div className="bg-stone-50 mt-20 flex gap-10">
-      <div className="1/4 min-h-max">
+    <div className="mt-20 flex gap-10 bg-yelloku duration-500 ease-in-out">
+      <div className="1/4 min-h-max duration-500 ease-in-out relative">
         <img
           src="https://images.unsplash.com/photo-1611918126831-0a8352d6196f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
           alt=""
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover brightness-50"
         />
+        <h1 className="text-5xl text-white font-bold mb-5 absolute inset-x-1/4 top-10">
+          Preparing the next <br /><span className="font-light">Travel</span>
+        </h1>
       </div>
-      <div className="w-3/4 mx-auto py-7">
-        <h1 className="text-3xl font-semibold">Preparing the next Travel</h1>
+      <div className="w-3/4 mx-auto px-16 py-10 duration-500 ease-in-out">
         <div className="flex items-center gap-10">
-          <label htmlFor="inputBudget" className="text-xl">
+          <label htmlFor="inputBudget" className="text-xl font-medium">
             Desired Budget :{" "}
           </label>
           <div>
             <input
               type="number"
               className="border-0 border-b-4 bg-transparent border-black"
+              placeholder="in Thousand"
             />
             IDR
           </div>
         </div>
 
+        <h1 className="text-2xl font-medium text-center mb-2">City</h1>
         <SwiperCar />
+        <h1 className="text-2xl font-medium text-center mb-2">Destination</h1>
+        <SwiperCar />
+
+        <div className="slider max-w-3xl mx-auto flex flex-col gap-3">
+          <div className="flex justify-between">
+            <label
+              htmlFor="default-range"
+              className="block mb-1 font-medium text-xl text-gray-900 dark:text-white">
+              Destination Allocation
+            </label>
+            <h1 className="text-lg">{sliderVal ? sliderVal + "%" : ""}</h1>
+          </div>
+
+          <input
+            id="default-range"
+            type="range"
+            onChange={(e) => setSliderVal(e.target.value)}
+            className="w-full h-2 bg-white rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+          />
+
+          <label htmlFor="numberDestination" className="font-medium text-xl">
+            Number of Destinations
+          </label>
+          <input
+            type="text"
+            id="numberDestination"
+            className="border-0 border-b-4 border-black bg-transparent"
+            placeholder="e.g : 5"
+          />
+        </div>
+
+        <button className="w-full text-white bg-black py-3 max-w-6xl mx-auto mt-10 block">
+          Generate
+        </button>
       </div>
     </div>
   )
