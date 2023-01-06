501c4573d09726ca226bc3c6788a8eb024e8f92e
alammooo <abdullah.alamm@gmail.com>
2023-01-06 16:43:46 +0700

    refactor travelstep page

--- src/pages/TravelStep.jsx
+++ src/pages/TravelStep.jsx
@@ -10,8 +10,8 @@ export default function TravelStep() {
     nav("/travelItenerary")
   }
   return (
-    <div className="mt-20 flex gap-10 bg-yelloku duration-500 ease-in-out">
-      <div className="1/4 min-h-max duration-500 ease-in-out relative">
+    <div className="mt-20 flex ease-in-out">
+      <div className="w-1/4 min-h-max ease-in-out relative">
         <img
           src="https://images.unsplash.com/photo-1611918126831-0a8352d6196f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
           alt=""
@@ -23,9 +23,9 @@ export default function TravelStep() {
         </h1>
       </div>
       <form
-        className="w-3/4 mx-auto px-16 py-10 duration-500 ease-in-out"
+        className="block w-3/4 mx-auto py-10 ease-in-out"
         onSubmit={(e) => handleSubmit(e)}>
-        <div className="flex items-center gap-10">
+        <div className="flex items-center gap-10 px-10">
           <label htmlFor="inputBudget" className="text-xl font-medium">
             Desired Budget :{" "}
           </label>
@@ -56,9 +56,9 @@ export default function TravelStep() {
 
           <input
             id="default-range"
-            type="range"
+            type="range"  
             onChange={(e) => setSliderVal(e.target.value)}
-            className="w-full h-2 bg-white rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+            className="w-full h-2 bg-stone-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
           />
 
           <label htmlFor="numberDestination" className="font-medium text-xl">
