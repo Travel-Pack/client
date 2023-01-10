01d7600964e34b96792204c004057d8e23ae244d
alammooo <abdullah.alamm@gmail.com>
2023-01-10 13:06:54 +0700

    change modal

--- src/pages/TravelStep.jsx
+++ src/pages/TravelStep.jsx
@@ -240,60 +240,39 @@ export default function TravelStep() {
               <button
                 onClick={() => setShowModal(!showModal)}
                 className="w-full text-lg font-medium underline text-black bg-yelloku py-3 mt-2 mx-auto block">
-                Generate
+                Set name for your travel
               </button>
+              <React.Fragment>
+                <Modal
+                  show={showModal}
+                  size="md"
+                  popup={true}
+                  onClose={() => setShowModal(!showModal)}>
+                  <Modal.Header />
+                  <Modal.Body>
+                    <div className="space-y-6 px-6 pb-4 sm:pb-6 lg:px-8 xl:pb-8">
+                      <div>
+                        <label
+                          htmlFor="travelPackName"
+                          className="text-center font-medium block mb-2">
+                          Give Travel Pack name
+                        </label>
+
+                        <TextInput id="travelPackName" type="name" required={true} />
+                      </div>
+                      <div className="w-full">
+                        <button className="bg-yelloku w-full py-2 font-medium active:scale-95 duration-100" type="submit">Generate</button>
+                      </div>
+                    </div>
+                  </Modal.Body>
+                </Modal>
+              </React.Fragment>
             </form>
           ) : (
             ""
           )}
         </div>
       </div>
-      <React.Fragment>
-        <Modal show={showModal} size="md" popup={true} onClose={() => showModal}>
-          <Modal.Header />
-          <Modal.Body>
-            <div className="space-y-6 px-6 pb-4 sm:pb-6 lg:px-8 xl:pb-8">
-              <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-                Sign in to our platform
-              </h3>
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="email" value="Your email" />
-                </div>
-                <TextInput id="email" placeholder="name@company.com" required={true} />
-              </div>
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="password" value="Your password" />
-                </div>
-                <TextInput id="password" type="password" required={true} />
-              </div>
-              <div className="flex justify-between">
-                <div className="flex items-center gap-2">
-                  <Checkbox id="remember" />
-                  <Label htmlFor="remember">Remember me</Label>
-                </div>
-                <a
-                  href="/modal"
-                  className="text-sm text-blue-700 hover:underline dark:text-blue-500">
-                  Lost Password?
-                </a>
-              </div>
-              <div className="w-full">
-                <Button>Log in to your account</Button>
-              </div>
-              <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                Not registered?{" "}
-                <a
-                  href="/modal"
-                  className="text-blue-700 hover:underline dark:text-blue-500">
-                  Create account
-                </a>
-              </div>
-            </div>
-          </Modal.Body>
-        </Modal>
-      </React.Fragment>
     </div>
   )
 }
