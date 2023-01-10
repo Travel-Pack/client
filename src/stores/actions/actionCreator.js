1d87baa5d2b742875fed324cd63eefa1d670c1be
Joshua Natanael <joshuanatanael@Joshuas-MacBook-Air.local>
2023-01-10 12:54:48 +0700

    add find destination function

--- src/stores/actions/actionCreator.js
+++ src/stores/actions/actionCreator.js
@@ -200,12 +200,33 @@ export function postReview(review) {
   }
 }
 
-export function fetcDestinations(params) {
+export function fetcDestinations(params, data) {
   return (dispatch, getState) => {
     let url = `${baseUrl}/publics/destinations`;
+    let addedFilter = false;
     if(params){
-      url += `?orderBy=${params}`
+      url += `?orderBy=${params}`;
+      addedFilter = true;
     }
+    if(data){
+      if(addedFilter){
+        url += "&";
+        addedFilter = false;
+      }
+      else{
+        url += "?";
+      }
+      for(let key in data){
+        if(data[key]){
+          if(addedFilter){
+            url += "&";
+            addedFilter = false;
+          }
+          url += `${key}=${data[key]}`;
+          addedFilter = true;
+        }
+      }
+    }console.log(url);
     return axios({
       method: "GET",
       url,
